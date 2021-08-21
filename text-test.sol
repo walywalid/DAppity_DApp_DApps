ce20eafeac3764856b4127f35daab1e786175f2b
Walid Brahim <59485576+walywalid@users.noreply.github.com>
2021-08-21 18:35:52 +0400

    Update text-test.sol

--- text-test.sol
+++ text-test.sol
@@ -1,23 +1,16 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2580
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+pragma solidity ^0.4.24;
 
-\f0\fs24 \cf0 pragma solidity ^0.4.24;\
-\
-contract test\{\
-    string value;\
-    \
-    constructor() public\{\
-        value = "theValue";\
-    \}\
-    \
-    function get() public returns(string)\{\
-        return value;\
-    \}\
-    function set(string _value) public\{\
-        value = _value;\
-    \}\
-\}}
+contract test{
+    string value;
+    
+    constructor() public{
+        value = "theValue";
+    }
+    
+    function get() public returns(string){
+        return value;
+    }
+    function set(string _value) public{
+        value = _value;
+    }
+}
