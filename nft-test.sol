4a08c80a764111df807795f498c1c10be82600b4
Walid Brahim <59485576+walywalid@users.noreply.github.com>
2021-08-23 20:05:11 +0400

    Add files via upload

---
+++ nft-test.sol
@@ -0,0 +1,20 @@
+pragma solidity 0.6.6;
+
+import "Your_token_directory" ;
+
+contract elever-nft-test is ERC721; {
+    uint256 publice tokenCounter; 
+    constructor () public ERC721("Vita Beta","VITA");{
+    tokenCounter= 0;
+}
+    funciton createCollictble(string memory tokenURI) public returns (uint256){
+        uint256 newItemID = tokenCounter; 
+        _safeMint(msg.sender,newItemID);
+        _etTokenUR(newItemID, tokenURI);
+        tokenCounter = tokenCounter + 1;
+        reutrn newItemID; 
+
+    }
+
+
+}
