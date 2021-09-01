5c41caba4855ed0a62b0a3c2d25dda4aa4b41fff
Walid Brahim <59485576+walywalid@users.noreply.github.com>
2021-09-01 23:01:14 +0400

    Change of token counter script

--- nft-test.sol
+++ nft-test.sol
@@ -5,7 +5,7 @@ import "Your_token_directory" ;
 contract elever-nft-test is ERC721; {
     uint256 publice tokenCounter; 
     constructor () public ERC721("Vita Beta","VITA");{
-    tokenCounter= 0;
+    tokenCounter= 1;
 }
     funciton createCollictble(string memory tokenURI) public returns (uint256){
         uint256 newItemID = tokenCounter; 
